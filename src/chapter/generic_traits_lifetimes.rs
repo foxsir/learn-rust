928c18fb330e307ed89900ab0a96cdbe06b2b32b
foxsir <838394225@qq.com>
2025-09-21 13:17:31 +0800

    update: update trait

--- src/chapter/generic_traits_lifetimes.rs
+++ src/chapter/generic_traits_lifetimes.rs
@@ -15,6 +15,39 @@ struct Point<T, B> {
     y: B,
 }
 
+trait Summary {
+    fn summarize(&self) -> String {
+        String::from("(Read more...)")
+    }
+}
+
+trait Display {
+    fn summarize2(&self) -> String {
+        String::from("(Read more...)")
+    }
+}
+
+impl Summary for Point<String, i32> {
+    fn summarize(&self) -> String {
+        format!("Point({}, {})", self.x, self.y)
+    }
+}
+
+impl Display for Point<String, i32> {}
+
+fn notify<T: Summary + Display>(item: &T) {
+    println!("{}", item.summarize2());
+}
+
+// 使用where范型
+fn some_function<T, U>(t: &T, u: &U) -> i32
+where
+    T: Display + Clone,
+    U: Clone,
+{
+    return 100;
+}
+
 pub fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
@@ -23,6 +56,9 @@ pub fn main() {
 
     let integer = Point::<String, i32> { x: String::from("Word"), y: 100 };
 
+    println!("{}", integer.summarize());
+
+    notify(&Point::<String, i32> { x: String::from("Word"), y: 100 });
 
     println!("{}", integer.x);
     println!("{}", integer.y);
