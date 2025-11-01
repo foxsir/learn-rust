b3550cd09d012d73c3aee8910174bba2a5842f3f
foxsir <838394225@qq.com>
2025-11-01 17:45:37 +0800

    update: update rust test

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -2,6 +2,10 @@ fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
+fn _greeting(name: &str) -> String {
+    format!("Hello {name}!")
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -9,12 +13,30 @@ mod tests {
     #[test]
     fn it_works() {
         let result = add(2, 2);
-        assert_eq!(result, 4);
+        assert_eq!(result, 5, "result result equal 4");
     }
+
+    #[test]
+    fn ass() {
+        // let result = greeting("xx");
+        assert!(false, "Hello xx?");
+    }
+
+    #[test]
+    #[should_panic] // 应该出现恐慌
+    fn sp() {
+        panic!("is panic");
+    }
+
 
     #[test]
     #[ignore]
     fn another() {
+        panic!("Make this test fail");
+    }
+
+    #[test]
+    fn another2() {
         panic!("Make this test fail");
     }
 }
